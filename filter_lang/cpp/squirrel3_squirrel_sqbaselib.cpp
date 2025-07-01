9bfdb489fd0acddb0db2c92ed50b0a111209d751
IchinaZiru <yutaihuangjing08@gmail.com>
2025-07-01 09:54:37 +0900

    Embed Source.

--- filter_lang/cpp/squirrel3_squirrel_sqbaselib.cpp
+++ filter_lang/cpp/squirrel3_squirrel_sqbaselib.cpp
@@ -429,8 +429,7 @@ static SQInteger number_delegate_tochar(HSQUIRRELVM v)
 
 /////////////////////////////////////////////////////////////////
 //TABLE DEFAULT DELEGATE
-
-static SQInteger table_rawdelete(HSQUIRRELVM v)
+Fstatic SQInteger table_rawdelete(HSQUIRRELVM v)
 {
     if(SQ_FAILED(sq_rawdeleteslot(v,1,SQTrue)))
         return SQ_ERROR;
